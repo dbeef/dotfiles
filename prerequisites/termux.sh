537b15a9054006b70916dbb0bc4dcbb01119c2b4
Daniel "dbeef" Zalega <daniel.zalega@gmail.com>
2021-11-08 21:43:56 +0100

    Updated prerequisites

--- prerequisites/termux.sh
+++ prerequisites/termux.sh
@@ -1,4 +1,25 @@
 #!/bin/bash -e
 
-# TODO
+echo "bell-character=ignore" >> ~/.termux/termux.properties
+
+pkg update
+pkg install openssh \
+            python \
+            vim-python \
+            tmux \
+            cmake \
+            git \
+            curl \
+            procps \
+            proot \
+            nodejs \
+            wget \
+            mc \
+            rust \
+            golang \
+            fzf \
+            man \
+            nmap-ncat \
+            gcc \
+            clang
 
--- prerequisites/ubuntu.sh
+++ prerequisites/ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo apt install -y cargo golang-go git cmake nodejs npm
+sudo apt update && sudo apt install -y cargo golang-go git cmake nodejs npm
 npm install yarn
 yarn install 
 
