927f4ee52c088d4a8522e06f13898bc89c6b4f9d
Daniel "dbeef" Zalega <daniel.zalega@gmail.com>
2021-11-08 23:28:58 +0100

    Added setup-termux.sh

---
+++ termux/setup-termux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+git clone --depth 1 --recursive https://github.com/adi1090x/termux-style
+cd termux-style
+./install.sh
+
+# Set color to base16-materia (89)
+# Set font to Hermit (20)
+
