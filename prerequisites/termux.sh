1a82deb4ef095b0ac5ccdee674b7d4b79608106d
Daniel "dbeef" Zalega <daniel.zalega@gmail.com>
2021-11-09 16:11:08 +0100

    fixup

--- prerequisites/termux.sh
+++ prerequisites/termux.sh
@@ -3,27 +3,27 @@
 mkdir -p ~/.termux/
 echo "bell-character=ignore" >> ~/.termux/termux.properties
 
-pkg update
-pkg install openssh \
-            python \
-            vim-python \
-            tmux \
-            cmake \
-            git \
-            curl \
-            procps \
-            proot \
-            nodejs \
-            wget \
-            mc \
-            rust \
-            golang \
-            fzf \
-            man \
-            nmap-ncat \
-            clang \
-            perl \
-            libicu
+pkg update || pkg install
+    openssh \
+    python \
+    vim-python \
+    tmux \
+    cmake \
+    git \
+    curl \
+    procps \
+    proot \
+    nodejs \
+    wget \
+    mc \
+    rust \
+    golang \
+    fzf \
+    man \
+    nmap-ncat \
+    clang \
+    perl \
+    libicu
 
 npm install -g yarn
 yarn install
