d5bf260046476ac999b5f3c1661ebb42e3272407
Daniel "dbeef" Zalega <daniel.zalega@gmail.com>
2021-11-09 17:15:44 +0100

    fixup

--- termux/setup-termux.sh
+++ termux/setup-termux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e 
 git clone --depth 1 --recursive https://github.com/adi1090x/termux-style
 cd termux-style
-./install.sh
+./install
 
 # Set color to 81/83/78/60/73 (nord/smycki/material/railscasts-dark/dracula)
 # Set font to Hermit (20)
