b01c8ad5f9e8ef9d03bce88fce90e1d70080b078
Daniel "dbeef" Zalega <daniel.zalega@gmail.com>
2021-11-09 14:50:13 +0100

    Clarification

--- termux/fix-tmux-fingers.sh
+++ termux/fix-tmux-fingers.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e 
+
+# Since /tmp/ is not writable under termux, replace all paths containing it:
+
 HOME_TMP=$HOME/.tmp
 mkdir $HOME_TMP
 HOME_TMP_ESCAPED=`echo $HOME_TMP | sed "s/\//\\\\\\//g"`
